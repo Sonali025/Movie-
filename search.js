4f736eed92795d123ce8e16a4ab93275492ee5f9
Sonali025 <sonalisagar2001@gmail.com>
2026-05-27 13:28:27 +0530

    Fixed search page and movie loading issue

--- search.js
+++ search.js
@@ -1,87 +1,115 @@
-window.onload = () => {
-  let param = new URLSearchParams(window.location.search);
-  let title = param.get("query");
-  console.log(title);
-
-  if (title && title.trim() !== "") {
-    searchMovie(title);
-  } else {
-    console.log("Movie not found");
-    let divTag = document.createElement("div");
-    divTag.className = "col-xl-3";
-    let h3Tag = document.createElement("h3");
-    h3Tag.innerText = "Movie not found.....!";
-    divTag.append(h3Tag);
-    document.querySelector(".row").append(divTag);
-  }
-};
 const searchMovie = (title) => {
+
   let xhr = new XMLHttpRequest();
+
   xhr.open(
     "GET",
     `https://api.themoviedb.org/3/search/movie?api_key=c45a857c193f6302f2b5061c3b85e743&language=en-US&query=${title}&page=1`
   );
 
   xhr.send();
-  //4 step
+
   xhr.onreadystatechange = () => {
+
     if (xhr.readyState == 4) {
+
       if (xhr.status == 200) {
+
         const movies = JSON.parse(xhr.responseText);
-        console.log(movies);
+
+        document.querySelector(".row").innerHTML = "";
+
         if (movies.results.length > 0) {
-  movies.results.forEach((movies) => {
-    console.log(movies);
-    console.log(movies.title);
-    console.log(movies.vote_average);
-    console.log(movies.poster_path);
 
-    let divTag = document.createElement("div");
-    divTag.className = "col-xl-3";
+          movies.results.forEach((movie) => {
 
-    let imgTag = document.createElement("img");
-    imgTag.className = "img-fluid";
+            let divTag = document.createElement("div");
 
-    let h4Tag = document.createElement("h4");
-    let pTag = document.createElement("p");
+            divTag.className = "col-xl-3 col-md-4 col-sm-6 mb-4";
 
-    imgTag.src = `https://image.tmdb.org/t/p/w500${movies.poster_path}`;
-    h4Tag.innerText = movies.title;
-    pTag.innerText = `Rating: ${movies.vote_average}`;
+            let imgTag = document.createElement("img");
 
-    divTag.append(imgTag);
-    divTag.append(h4Tag);
-    divTag.append(pTag);
+            imgTag.className = "img-fluid";
 
-    document.querySelector(".row").append(divTag);
-  });
-} else {
-  console.log("Movie not found");
+            let h4Tag = document.createElement("h4");
 
-  let divTag = document.createElement("div");
-  divTag.className = "col-xl-3";
+            let pTag = document.createElement("p");
 
-  let h3Tag = document.createElement("h3");
-  h3Tag.innerText = "Movie not found.....!";
+            imgTag.src = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-  divTag.append(h3Tag);
+            h4Tag.innerText = movie.title;
 
-  document.querySelector(".row").append(divTag);
-}
+            pTag.innerText = `Rating: ${movie.vote_average}`;
+
+            divTag.append(imgTag);
+
+            divTag.append(h4Tag);
+
+            divTag.append(pTag);
+
+            document.querySelector(".row").append(divTag);
+
+          });
+
+        } else {
+
+          let divTag = document.createElement("div");
+
+          divTag.className = "col-12 text-center";
+
+          let h3Tag = document.createElement("h3");
+
+          h3Tag.innerText = "Movie not found.....!";
+
+          divTag.append(h3Tag);
+
+          document.querySelector(".row").append(divTag);
+
+        }
+
       } else {
+
         console.log("Error: " + xhr.status);
+
       }
+
     }
+
   };
+
 };
 
 
 const handleSearch = (event) => {
+
   event.preventDefault();
 
   let query = document.querySelector(".searchBox").value;
 
-  if(query && query.trim() !== ""){
+  if (query && query.trim() !== "") {
+
     window.location.href = `search.html?query=${query}`;
+
   }
+
 };
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Run only on search page
+  if (window.location.pathname.includes("search.html")) {
+
+    let param = new URLSearchParams(window.location.search);
+
+    let title = param.get("query");
+
+    if (title && title.trim() !== "") {
+
+      searchMovie(title);
+
+    }
+
+  }
+
+});
